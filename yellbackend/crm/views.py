d6c4f903ee7ee8e6fd00c757d8d6ca4b8e3f4ece
验钞 <lyanchaolee@gmail.com>
2016-04-17 19:01:51 +0800

    updatatataeat

--- yellbackend/crm/views.py
+++ yellbackend/crm/views.py
@@ -25,10 +25,34 @@ def login_post(request):
 	sales=Sales.objects.get(username=request.POST.get('username'))
 	passinput=request.POST.get('password')
 	if check_password(passinput,sales.password):
-		return HttpResponse("LOGIN OK")
+		template=loader.get_template('crm/leads.html')
+		response=HttpResponse(template.render(RequestContext(request)))
+		response.set_cookie("is_login","True")
+		response.set_cookie("user_id",sales.id)
+		return response
 	else:
 		return HttpResponse("LOGIN FAIL"+sales.password+passinput)
 		
+def get_leads(sales_identify):
+
+	customer_list=Customer.objects.filer(sales_id=sales_identify).order_by('-id')
+	for customer in customer_list:
+		all_remark=Remark.objects.filter(refer_id=customer.id, refer_type='Leads').order_by('id')
+		if all_remark.count() > 0:
+			first_remark=all_remark[0]
+			customer.remark_list=all_remark[1:]
+			customer.remark_size=all_remark.count()
+			customer.first_remark=first_remark
+		else:
+			first_remark=Remark()
+			first_remark.remark=""
+			customer.first_remark=""
+		
+	template=loader.get_template('crm/leads.html')
+	context={
+		'customer_list':customer_list
+	}
+	return context
 		
 def register_post(request):
 	passwd=make_password(request.POST.get('password'),'yell','pbkdf2_sha256')
