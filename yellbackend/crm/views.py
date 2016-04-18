55c0fca925d93a363be4105f92e601d0280c899e
验钞 <lyanchaolee@gmail.com>
2016-04-18 22:07:08 +0800

    add cookie check

--- yellbackend/crm/views.py
+++ yellbackend/crm/views.py
@@ -6,6 +6,7 @@ from .models import Sales
 from django.contrib.auth.hashers import make_password,check_password
 from django.core.urlresolvers import reverse
 from django.contrib.auth.views import logout as django_logout
+from django.shortcuts import get_object_or_404, render
 
 def course_mgt(request):
 	customer_list=Customer.objects.order_by('-id')[:5]
@@ -45,32 +46,32 @@ def login_post(request):
 
 
 def leads(request):
-	sales_identify=request.COOKIES["user_id"]
-	sales=Sales.objects.get(id=sales_identify)
-	if sales.is_super=='Y':
-		customer_list=Customer.objects.order_by('-id')
-	else:
-		customer_list=Customer.objects.filter(sales_id=sales_identify).order_by('-id')
-	
-	for customer in customer_list:
-		all_remark=Remark.objects.filter(refer_id=customer.id, refer_type='Leads').order_by('id')
-		if all_remark.count() > 0:
-			first_remark=all_remark[0]
-			customer.remark_list=all_remark[1:]
-			customer.remark_size=all_remark.count()
-			customer.first_remark=first_remark
+	try:
+		sales_identify=request.COOKIES["user_id"]
+		sales=Sales.objects.get(id=sales_identify)
+		if sales.is_super=='Y':
+			customer_list=Customer.objects.order_by('-id')
 		else:
-			first_remark=Remark()
-			first_remark.remark=""
-			customer.first_remark=""
-		
-	template=loader.get_template('crm/leads.html')
-	context={
-		'customer_list':customer_list,
-		'sales_id':sales_identify
-	}
-	
-	return HttpResponse(template.render(context, request))
+			customer_list=Customer.objects.filter(sales_id=sales_identify).order_by('-id')
+		for customer in customer_list:
+			all_remark=Remark.objects.filter(refer_id=customer.id, refer_type='Leads').order_by('id')
+			if all_remark.count() > 0:
+				first_remark=all_remark[0]
+				customer.remark_list=all_remark[1:]
+				customer.remark_size=all_remark.count()
+				customer.first_remark=first_remark
+			else:
+				first_remark=Remark()
+				first_remark.remark=""
+				customer.first_remark=""
+		template=loader.get_template('crm/leads.html')
+		context={
+			'customer_list':customer_list,
+			'sales_id':sales_identify
+		}
+		return HttpResponse(template.render(context, request))
+	except(KeyError):
+		return HttpResponse("You don't have privileges")		
 
 
 def member(request):
